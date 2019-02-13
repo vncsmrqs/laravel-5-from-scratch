398167333db60b9aa89ed11d51e46ca878391d82
Vinicius Marques <vncsmrqs@gmail.com>
2019-02-13 13:29:32 -0200

    Episode 4 - Routing to Controllers

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class PagesController extends Controller
+{
+    public function home()
+    {
+        $people = ['Taylor', 'Matt'. 'Jeffrey'];
+        return view('welcome', compact('people'));
+    }
+
+    public function about()
+    {
+        return view('pages.about');
+    }
+
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,22 +1,10 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+// Route::get('/', function () {
+//     $people = ['Taylor', 'Matt'. 'Jeffrey'];
+//     return view('welcome', compact('people'));
+// });
 
-Route::get('/', function () {
-    $people = ['Taylor', 'Matt'. 'Jeffrey'];
-    return view('welcome', compact('people'));
-});
+Route::get('/', 'PagesController@home');
 
-Route::get('about', function()
-{
-    return view('pages.about');
-});
+Route::get('about', 'PagesController@about');
