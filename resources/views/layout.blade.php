1a226c0faded6b6ce7da68a5237d99bccdb2cf0b
Vinicius Marques <vncsmrqs@gmail.com>
2019-02-13 14:15:02 -0200

    Episode 6 - How to Manage Your CSS and JS

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,7 +6,9 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
+    <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
+    
+    @yield('header')
 </head>
 <body>
 
